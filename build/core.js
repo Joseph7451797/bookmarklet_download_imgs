0362a8c62eac091cd29a04f89af23292fc42fe6d
joseph <joseph.chang@appimc.com>
2016-05-06 19:26:54 +0800

    webpack host mode ip use environment value

--- build/core.js
+++ build/core.js
@@ -1,1 +1,129 @@
-!function(e){function o(i){if(r[i])return r[i].exports;var l=r[i]={exports:{},id:i,loaded:!1};return e[i].call(l.exports,l,l.exports,o),l.loaded=!0,l.exports}var r={};return o.m=e,o.c=r,o.p="/build/",o(0)}([function(e,o,r){e.exports=r(1)},function(e,o){!function(e){var o,r=e.querySelectorAll("img"),i=r.length?r.length:null,l=(new Date).getTime(),n="<div id="+l+"_close style=display: inline-block;margin-bottom: 15px;width: 40px;height: 40px;background-color: #333;color: #fff;line-height: 40px;font-size: 15px;text-align: center;border-radius: 3px;cursor: pointer;>關閉</div>";if(i){for(var t=0;i>t;t++){var d=r[t].src?r[t].src:null;if(d&&d.search("http")>-1){var a=d.lastIndexOf(".");a>-1&&(n+='<img src="'+d+'" style="display: block;max-width: 100%;"/><a id="'+l+'_download_link" href="'+d+'" download="img-'+t+d.substr(a)+'" style="display: block;margin: 10px 0 25px; color: blue;">'+d+"</a>")}}n+="抓圖工具頁面 by Joseph, inspired by Angryplay.blogspot.tw"}else n+="本網頁沒有圖片可供下載<br/>抓圖工具頁面 by Joseph, inspired by Angryplay.blogspot.tw";o=e.createElement("div"),o.id=l+"_for_download_window";var p={position:"fixed",zIndex:"10000",top:"20px",right:"20px",padding:"20px",width:"33%",height:"500px",minWidth:"300px",overflow:"auto",backgroundColor:"#fff",boxShadow:"2px 2px 8px 4px #dfdfdf",borderRadius:"10px"};for(var s in p)o.style[s]=p[s];o.innerHTML=n,e.body.insertBefore(o,e.body.firstChild);var c=e.querySelectorAll('a[id="'+l+'_download_link"]').length?e.querySelectorAll('a[id="'+l+'_download_link"]'):null,f=e.querySelector('div[id="'+l+'_close"]');if(f.onclick=function(){e.getElementById(l+"_for_download_window").remove()},c)for(var x=c.length,t=0;x>t;t++){var u=e.createEvent("MouseEvents");u.initMouseEvent("click",!0,!0,window,0,0,0,0,0,!1,!1,!1,!1,0,null)}}(document)}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/Users/Kuo/Documents/experiments/bookmarklet_download_imgs/build/";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	(function(s) {
+		var imgs = s.querySelectorAll('img');
+		var length = imgs.length ? imgs.length : null;
+		var hash = new Date().getTime();
+		var temp = '<div id=' + hash + '_close style=display: inline-block;margin-bottom: 15px;width: 40px;height: 40px;background-color: #333;color: #fff;line-height: 40px;font-size: 15px;text-align: center;border-radius: 3px;cursor: pointer;>關閉</div>';
+		var d;
+
+		if( length ) {
+			for(var i = 0; i < length; i++) {
+
+				var src = imgs[i].src ? imgs[i].src : null;
+
+				if( src && src.search('http') > -1) {
+					var k = src.lastIndexOf('.');
+					if( k > -1 ) {
+						temp += '<img src="' + src + '" style="display: block;max-width: 100%;"/><a id="' + hash + '_download_link" href="' + src + '" download="img-' + i + src.substr(k) + '" style="display: block;margin: 10px 0 25px; color: blue;">' + src + '</a>';
+					}
+				}
+			}
+
+			temp += '抓圖工具頁面 by Joseph, inspired by Angryplay.blogspot.tw';
+
+		}else {
+			temp += '本網頁沒有圖片可供下載<br/>抓圖工具頁面 by Joseph, inspired by Angryplay.blogspot.tw';
+		}
+
+		d = s.createElement('div');
+		d.id = hash + '_for_download_window';
+
+		var styleObj = {
+	      'position': 'fixed',
+	      'zIndex': '10000',
+	      'top': '20px',
+	      'right': '20px',
+	      'padding': '20px',
+	      'width': '33%',
+	      'height': '500px',
+	      'minWidth': '300px',
+	      'overflow': 'auto',
+	      'backgroundColor': '#fff',
+	      'boxShadow': '2px 2px 8px 4px #dfdfdf',
+	      'borderRadius': '10px'
+	    };
+
+	    for(var key in styleObj) {
+	      d.style[key] = styleObj[key];
+		}
+		
+		d.innerHTML = temp;
+		
+		s.body.insertBefore(d, s.body.firstChild);
+
+		var atags = s.querySelectorAll('a[id="' + hash + '_download_link"]').length ? s.querySelectorAll('a[id="' + hash + '_download_link"]') : null;
+		var closebtn = s.querySelector('div[id="' + hash + '_close"]');
+
+		closebtn.onclick = function() {
+			s.getElementById(hash + '_for_download_window').remove();
+		}
+
+		if( atags ) {
+
+			var alength = atags.length;
+
+			for(var i = 0; i < alength; i++){
+				//建立觸發事件
+				var e = s.createEvent('MouseEvents');
+				e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+				//觸發下載(chrome會跳出視窗請允許大量下載)
+				//a[i].dispatchEvent(e);
+			}
+		}
+		
+	})(document);
+
+/***/ }
+/******/ ]);
