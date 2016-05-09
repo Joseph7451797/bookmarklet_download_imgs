4c9dcb117881200b93450c1710069e378d03686e
Joseph Chang <Joseph@JosephdeMacBook-Pro.local>
2016-05-10 00:08:40 +0800

    批次下載檔案完成，firefox creatEvent 需改寫法

--- download_core.js
+++ download_core.js
@@ -2,7 +2,8 @@
 	var imgs = s.querySelectorAll('img');
 	var length = imgs.length ? imgs.length : null;
 	var hash = new Date().getTime();
-	var temp = '<div id=' + hash + '_close style="position: fixed;display: inline-block;margin: -10px 0 20px -10px;width: 45px;background-color: #333;color: #fff;line-height: 40px;font-size: 15px;text-align: center;border-radius: 3px;box-shadow: 1px 1px 4px 1px rgba(127, 127, 127, 0.8);cursor: pointer";>關閉</div>';
+	var temp = '<div id=' + hash + '_close style="position: fixed;z-index: 1;top: 30px;display: inline-block;width: 45px;background-color: #333;color: #fff;line-height: 40px;font-size: 15px;text-align: center;border-radius: 3px;box-shadow: 1px 1px 4px 1px rgba(127, 127, 127, 0.8);cursor: pointer";>關閉</div>'
+			+'<div id=' + hash + '_download_btn style="position: fixed;z-index: 1;top: 30px;right: 30px;display: inline-block;padding: 0 10px;background-color: #7f7fff;color: #fff;line-height: 40px;font-size: 15px;text-align: center;border-radius: 3px;box-shadow: 1px 1px 6px 1px rgba(127, 127, 127, 0.5);cursor: pointer">下載選擇圖片</div>';
 	var d;
 
 	if( length ) {
@@ -13,7 +14,7 @@
 			if( src && src.search('http') > -1) {
 				var k = src.lastIndexOf('.');
 				if( k > -1 ) {
-					temp += '<div style="margin: 10px 0 40px;max-width: 100%;"><img src="' + src + '" style="display: block;margin-bottom: 10px;max-width: 100%;"/><a id="' + hash + '_download_link" href="' + src + '" download="img-' + i + src.substr(k) + '" style="color: blue;font-size: 16px;">下載此圖</a></div>';
+					temp += '<div style="position: relative;margin: 10px 0 40px;max-width: 100%;"><img src="' + src + '" style="display: block;margin-bottom: 10px;max-width: 100%;"/><input type="checkbox" name="imgcheckboxes" style="position: absolute;top: 0;left: 0;"><a id="' + hash + '_download_link" href="' + src + '" download="img-' + i + src.substr(k) + '" style="color: blue;font-size: 16px;">下載此圖</a></div>';
 				}
 			}
 		}
@@ -32,7 +33,7 @@
       'zIndex': '10000',
       'top': '20px',
       'right': '20px',
-      'padding': '20px',
+      'padding': '50px 20px 20px',
       'width': '33%',
       'height': '500px',
       'minWidth': '300px',
@@ -52,6 +53,7 @@
 
 	var atags = s.querySelectorAll('a[id="' + hash + '_download_link"]').length ? s.querySelectorAll('a[id="' + hash + '_download_link"]') : null;
 	var closebtn = s.querySelector('div[id="' + hash + '_close"]');
+	var downloadBtn = s.querySelector('div[id="' + hash + '_download_btn"]');
 
 	closebtn.onclick = function() {
 		s.getElementById(hash + '_for_download_window').remove();
@@ -65,8 +67,17 @@
 			//建立觸發事件
 			var e = s.createEvent('MouseEvents');
 			e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-			//觸發下載(chrome會跳出視窗請允許大量下載)
-			//a[i].dispatchEvent(e);
+		}
+	}
+
+	downloadBtn.onclick = function() {
+		var arr = [];
+		var checkedBoxes = s.querySelectorAll('input[name=imgcheckboxes]:checked');
+		for(var j = 0; j < checkedBoxes.length; j++) {
+			arr.push(checkedBoxes[j].nextSibling);
+		}
+		for(var l = 0; l < arr.length; l++) {
+			arr[l].click();
 		}
 	}
 	
